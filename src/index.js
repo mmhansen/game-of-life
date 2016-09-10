51956744c061837e11ae0392410db56e247b092a
Michael Hansen <hansen.mmb@gmail.com>
2016-09-10 14:18:58 -0400

    added test module to make sure master is working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // Set up your application entry point here...
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render (){
+    return (
+      <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-12 col-md-4 offset-md-4">
+        herro world
+        </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
